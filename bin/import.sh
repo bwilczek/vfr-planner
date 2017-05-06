ae8d322b285fa9a64d738d274bf46cea6c5ebe7c
Bartek Wilczek <bwilczek@gmail.com>
2017-05-06 19:39:53 +0200

    Progress on data import

---
+++ bin/import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APP_DIR=$BIN_DIR/..
+
+if [ -z "$ONEDRIVE_DATA_URL" ]; then
+  echo "ERROR: ENV variable ONEDRIVE_DATA_URL not set" >&2
+  exit 1
+fi
+
+docker run --rm \
+  -u $(id -u $USER):$(id -g $USER) \
+  -e HOME=/tmp \
+  -v $APP_DIR/import:/download \
+  onedrive $ONEDRIVE_DATA_URL
