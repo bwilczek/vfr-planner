4b5eac1ccbdd05fed9d6c5a50332aaf42185344c
Bartek Wilczek <bwilczek@gmail.com>
2017-06-10 21:34:25 +0200

    Importing airspaces from atmavio resources

--- app/models/airspace.rb
+++ app/models/airspace.rb
@@ -1,4 +1,4 @@
 class Airspace < ApplicationRecord
-  enum kind: [:fis, :atz, :ctr, :mctr, :matz, :p, :r, :d, :tra, :tsa, :ea, :tma, :mrt, :trf, :rmz, :adiz, :other]
+  enum kind: [:fis, :atz, :ctr, :mctr, :matz, :prohibited, :restricted, :danger, :tra, :tsa, :ea, :tma, :mrt, :tfr, :rmz, :adiz, :other, :notam_point]
 
 end
--- app/models/import_atmavio.rb
+++ app/models/import_atmavio.rb
@@ -2,7 +2,7 @@ class ImportAtmavio < ApplicationRecord
 
   GPX_URL_NAV_POINTS = 'https://oc.atmavio.pl/index.php/s/UVlhkwKVmTb2lHm/download'
 
-  STATUS_MAP = {
+  NAV_POINT_KIND_MAP = {
     'Lądowisko zarejestrowane' => :uncontrolled,
     'Polecone zagraniczne' => :uncontrolled,
     'Lotnisko' => :uncontrolled,
@@ -12,6 +12,48 @@ class ImportAtmavio < ApplicationRecord
     'Drogowy Odcinek Lotniskowy' => :other_airstrip,
     'Lądowisko trudne' => :other_airstrip,
     'Lądowisko niepotwierdzone' => :other_airstrip,
+  }
+
+  AIRSPACE_KIND_MAP = {
+    '#AAA' => :other,
+    '#ADIZ' => :adiz,
+    '#ATZ' => :atz,
+    '#CTR' => :ctr,
+    '#EPD' => :danger,
+    '#EPP' => :prohibited,
+    '#EPR' => :restricted,
+    '#FIS' => :fis,
+    '#MATZ' => :matz,
+    '#Miejscowość' => :notam_point,
+    '#MRT' => :mrt,
+    '#NWA' => :other,
+    '#RMZ' => :rmz,
+    '#ROL' => :ea,
+    '#TFR' => :tfr,
+    '#TMA' => :tma,
+    '#TRA' => :tra,
+    '#TSA' => :tsa,
+  }
+
+  AIRSPACE_PERMANENT_MAP = {
+    '#AAA' => false,
+    '#ADIZ' => true,
+    '#ATZ' => false,
+    '#CTR' => true,
+    '#EPD' => false,
+    '#EPP' => true,
+    '#EPR' => true,
+    '#FIS' => true,
+    '#MATZ' => false,
+    '#Miejscowość' => false,
+    '#MRT' => false,
+    '#NWA' => false,
+    '#RMZ' => false,
+    '#ROL' => false,
+    '#TFR' => false,
+    '#TMA' => true,
+    '#TRA' => false,
+    '#TSA' => false,
   }
 
   def self.parse_and_set_atmavio_property(nav_point, key, value)
@@ -26,7 +68,7 @@ class ImportAtmavio < ApplicationRecord
     when 'Radio'
       nav_point.radio = value
     when 'Status'
-      nav_point.kind = STATUS_MAP[value]
+      nav_point.kind = NAV_POINT_KIND_MAP[value]
     end
   end
 
@@ -64,6 +106,38 @@ class ImportAtmavio < ApplicationRecord
       sleep 0.05
     end
 
+    puts "Finished at #{Time.now}"
+  end
+
+  ##
+  # Example: echo 'ImportAtmavio.import_airspaces_all("/some/path/vfr-planner/import")' | bundle exec rails console
+  #
+  def self.import_airspaces_all(import_directory)
+    puts "Started at #{Time.now}"
+    kml_path = File.join(import_directory, 'Strefy wszystkie.kml')
+    puts "Processing airspaces list"
+    xml = Nokogiri::XML(File.open(kml_path))
+    xml.xpath('//xmlns:Folder/xmlns:Placemark').each do |placemark|
+      puts "================"
+      description = placemark.xpath('./xmlns:description').text.strip
+      style_url = placemark.xpath('.//xmlns:styleUrl').text.strip
+
+      levels_raw, description = description.split("\n", 2)
+      level_min, level_max = levels_raw.split(',').map{|a| a.gsub(/\D/, '').to_i}
+
+      airspace = Airspace.new
+      airspace.name = placemark.xpath('./xmlns:name').text.strip
+      airspace.description = description
+      airspace.level_min = level_min
+      airspace.level_max = level_max
+      airspace.country = 'pl'
+      airspace.points = placemark.xpath('.//xmlns:coordinates').text.strip
+      airspace.kind = AIRSPACE_KIND_MAP[style_url]
+      airspace.permanent = AIRSPACE_PERMANENT_MAP[style_url]
+
+      puts airspace.name
+      airspace.save
+    end
     puts "Finished at #{Time.now}"
   end
 
