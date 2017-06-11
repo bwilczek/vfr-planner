633d2a7347b17d9364ee375bcc69c298364bc841
Bartek Wilczek <bwilczek@gmail.com>
2017-06-11 15:06:37 +0200

    Added filtering airspaces by levels

--- client/src/js/selectors/airspaces.js
+++ client/src/js/selectors/airspaces.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect'
+import { filter } from 'lodash'
 
 const airspacesSelector = state => state.airspaces
 const uiSelector = state => state.ui
@@ -8,6 +9,9 @@ export const getAirspacesForFilters = createSelector(
   uiSelector,
   (airspaces, ui) => {
     // TODO:  filter by ui.levels[0] / ui.levels[1] / ui.hours[0] / ui.hours[1]
-    return airspaces
+    return filter(airspaces, (airspace) => {
+      return ( airspace.level_min >= ui.levels[0] && airspace.level_min <= ui.levels[1] )
+        || ( airspace.level_max >= ui.levels[0] && airspace.level_max <= ui.levels[1] )
+    })
   }
 )
