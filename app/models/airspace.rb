0d5d09b73b8f73da3a6e3855ff34acd2f4e9021f
Bartek Wilczek <bwilczek@gmail.com>
2017-06-17 18:53:28 +0200

    Cascade deleting active airspaces along with all airspaces

--- app/models/airspace.rb
+++ app/models/airspace.rb
@@ -1,5 +1,5 @@
 class Airspace < ApplicationRecord
-  has_many :active_airspace
+  has_many :active_airspace, :dependent => :delete_all
   enum kind: [:fis, :atz, :ctr, :mctr, :matz, :prohibited, :restricted, :danger, :tra, :tsa, :ea, :tma, :mrt, :tfr, :rmz, :adiz, :other, :notam_point]
 
 end
