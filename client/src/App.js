2c92e7f34c7ebed9d0ea53e95248f9c22b0b2ad7
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-06-09 16:46:33 +0200

    WIP introduce react-intl

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from 'reactn'
+import axios from 'axios'
+import { FormattedMessage } from 'react-intl'
 
 export default class App extends React.PureComponent {
-  componentDidMount() {
+  componentWillMount() {
     this.setGlobal(
-      fetch('/api/nav_points?countries[]=pl&kinds[]=vfr_point')
-        .then(response => response.json())
-        .then(points => ({ point: points[0] }))
+      axios.get('/api/nav_points?countries[]=pl&kinds[]=vfr_point')
+        .then(response => ({point: response.data[0]}))
         .catch(err => ({ error: err }))
     )
-    // this.setGlobal({point: 'Kasia'})
+    this.setGlobal(
+      axios.post('/api/intl', {locale: this.global.locale})
+        .then(response => ({translations: {pl: response.data.messages}}))
+        .catch(err => ({ error: err }))
+    )
   }
 
   render() {
-    console.log('render', this.global)
     return (
       <>
         <div id="point">{this.global.point.name}</div>
         <div>{this.global.error}</div>
+        <FormattedMessage id="navAids" />
       </>
     )
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { setGlobal } from 'reactn'
+import { setGlobal, getGlobal } from 'reactn'
+import { IntlProvider } from 'react-intl'
 import addReactNDevTools from 'reactn-devtools'
 import ReactDOM from 'react-dom'
 import App from './App'
 
 setGlobal({
+  locale: 'pl',
+  translations: {'pl': {navAids: 'asdad'}},
   point: {name: 'loading...'},
   error: 'none'
 })
 
 addReactNDevTools()
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <IntlProvider locale="pl" messages={getGlobal().translations}>
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+)
