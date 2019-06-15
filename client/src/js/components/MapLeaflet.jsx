57ab6cfa9771df268bdd01feb3847f6f1d5c88d4
Monika Demichowicz <mondem@poczta.onet.pl>
2019-06-15 17:05:54 +0200

    WiP, leaflet map added (Hello world!)

--- client/src/js/components/MapLeaflet.jsx
+++ client/src/js/components/MapLeaflet.jsx
@@ -8,10 +8,20 @@ export default class MapLeaflet extends React.Component {
   }
 
   render() {
-    return (
-      <div ref="mapLeaflet" id="mapLeaflet">
-        Mapa!
-      </div>
-    )
-  }
+     const position = [51, 17];
+     return (
+       <Map center={position} zoom={13}>
+         <TileLayer
+           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+         />
+         <Marker position={position}>
+           <Popup>
+             A pretty CSS3 popup. <br/> Easily customizable.
+           </Popup>
+         </Marker>
+       </Map>
+     );
+   }
+
 }
