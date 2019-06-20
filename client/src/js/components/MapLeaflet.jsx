1c63bd7d1504838e861bf593b3e4874f51168a92
Monika Demichowicz <mondem@poczta.onet.pl>
2019-06-20 17:01:24 +0200

    Navigation points: exact route + context menu

--- client/src/js/components/MapLeaflet.jsx
+++ client/src/js/components/MapLeaflet.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 import { injectIntl } from 'react-intl'
 import { isEqual, forEach, random } from 'lodash'
-import { Map, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, Popup} from 'react-leaflet'
 import * as L from 'leaflet'
 
 import { getAirspacesForFilters } from '../selectors/airspaces'
 import { getNavigationData } from '../selectors/navigationData'
-import { addWaypointWithName } from '../actions/flightPlanActions'
+import { addWaypoint, addWaypointWithName } from '../actions/flightPlanActions'
+import * as format from '../lib/Formatter'
 import { getIconForNavPointKind, createAirspaceRawPolygon } from '../lib/MapUtils'
 
 @injectIntl
@@ -26,6 +27,9 @@ import { getIconForNavPointKind, createAirspaceRawPolygon } from '../lib/MapUtil
     return {
       addWaypointWithName: (waypoint, position = null) => {
         dispatch(addWaypointWithName(waypoint, position))
+      },
+      addWaypoint: (waypoint, position = null) => {
+        dispatch(addWaypoint(waypoint, position))
       }
     }
   }
@@ -62,6 +66,43 @@ export default class MapLeaflet extends React.Component {
     }
   }
 
+  onMarkerClick(marker) {
+    console.log ("click on marker ", marker)
+    console.log ("click on marker.title ", marker.options.title)
+    console.log ("click on marker.navPoint ", marker.navPoint)
+    let name = marker.navPoint.icao_code ? marker.navPoint.icao_code : marker.navPoint.name
+    this.props.addWaypoint({
+      name: name,
+      radio: marker.navPoint.radio,
+      elevation: marker.navPoint.elevation,
+      declination: marker.navPoint.declination,
+      latLng: marker.getLatLng(),
+      coords: format.coords(marker.getLatLng()),
+      key: `${random(10000, 99999)}-${Date.now()}`
+    })
+  }
+
+  onMarkerRightClick(marker) {
+    console.log ("RIGHT click on marker/map ", marker,this.map)
+    const { formatMessage } = this.props.intl
+    let content = `
+      <strong>${marker.navPoint.icao_code || ''} ${marker.navPoint.name}</strong><br />
+      ${formatMessage({id: 'navPointKind_' + marker.navPoint.kind})}<br />`
+
+    if (marker.navPoint.description) {
+      content += `<hr />${marker.navPoint.description}`
+    }
+
+    if (marker.navPoint.radio) {
+      content += marker.navPoint.radio
+    }
+
+    const popup = L.popup()
+    .setLatLng(marker.getLatLng())
+    .setContent(content)
+    .openOn(this.refs.leafletMap.leafletElement);
+  }
+
   plotNavPoints() {
     // CLEAR navPointMarkers
     forEach(this.navPointMarkers, (marker) => {
@@ -76,12 +117,12 @@ export default class MapLeaflet extends React.Component {
 
   createNavPointMarker(navPoint) {
     const latLng = L.latLng(navPoint.lat, navPoint.lng)
-    const icon = L.icon({iconUrl: getIconForNavPointKind(navPoint.kind)})
-    const newMarker = L.marker(latLng, {icon: icon, title: navPoint.name});
-    newMarker.addTo(this.refs.leafletMap.leafletElement);
-    //TODO@mondem
-    // newMarker.addListener('rightclick', this.onMarkerRightClick.bind(this, newMarker))
-    // newMarker.addListener('click', this.onMarkerClick.bind(this, newMarker))
+    const icon = L.icon({iconUrl: getIconForNavPointKind(navPoint.kind), iconAnchor: [12, 12]})
+    const newMarker = L.marker(latLng, {icon: icon, title: navPoint.name})
+    newMarker.navPoint = navPoint
+    newMarker.addTo(this.refs.leafletMap.leafletElement)
+    newMarker.on('click', this.onMarkerClick.bind(this, newMarker))
+    newMarker.on('contextmenu', this.onMarkerRightClick.bind(this, newMarker))
     return newMarker
   }
 
@@ -118,7 +159,6 @@ export default class MapLeaflet extends React.Component {
   initMap() {
     this.poly = L.polyline(this.props.waypoints.map((wp) => wp.latLng));
     this.poly.addTo(this.refs.leafletMap.leafletElement);
-
   }
 
   onMapClick(e) {
