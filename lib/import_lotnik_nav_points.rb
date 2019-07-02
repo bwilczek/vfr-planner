144769ff8c285985e83eeb13651c53dfec4b8584
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-07-02 08:59:22 +0200

    Improve nav_point.kind evaluation

--- lib/import_lotnik_nav_points.rb
+++ lib/import_lotnik_nav_points.rb
@@ -2,14 +2,29 @@ require 'see_you_cup'
 
 class ImportLotnikNavPoints
   KIND_MAP = {
-   'Waypoint' => :vfr_point,
-   'Airfield with grass surface runway' => :uncontrolled,
-   'Outlanding' => :airstrip,
-   'Airfield with solid surface runway' => :controlled
+    'Waypoint' => :vfr_point,
+    'Airfield with grass surface runway' => :uncontrolled,
+    'Outlanding' => :airstrip,
+    'Airfield with solid surface runway' => :controlled
+  }.freeze
+
+[:controlled, :uncontrolled, :military, :airstrip, :other_airstrip]
+
+  STATUS_MAP = {
+    'Drogowy Odcinek Lotniskowy' => :other_airstrip,
+    'Ladowisko' => :airstrip,
+    'Ladowisko niepotwierdzone' => :other_airstrip,
+    'Ladowisko trudne' => :other_airstrip,
+    'Ladowisko zarejestrowane' => :uncontrolled,
+    'Lotnisko' => :uncontrolled,
+    'Lotnisko kontrolowane' => :controlled,
+    'Lotnisko wojskowe' => :military,
+    'Lądowisko' => :airstrip
   }.freeze
 
   def initialize(path = nil)
     @path = path || "#{__dir__}/../import/Nasze_Trawniki_PunktyVFR_PL.cup"
+    @statuses = []
   end
 
   def run
@@ -21,7 +36,6 @@ class ImportLotnikNavPoints
       nav_point = NavPoint.new
       nav_point.name = wp.name
       nav_point.icao_code = wp.code
-      nav_point.kind = KIND_MAP[wp.style]
       nav_point.lat = wp.lat
       nav_point.lng = wp.lon
       nav_point.elevation = wp.elev
@@ -30,7 +44,15 @@ class ImportLotnikNavPoints
       nav_point.country = 'pl'
       nav_point.status = :active
       nav_point.declination = MagDeclination.get_declination(nav_point)
+      nav_point.kind = get_kind(wp)
       nav_point.save
     end
+    puts @statuses.sort.uniq
+  end
+
+  def get_kind(wp)
+    status = wp.desc.match(/Status: ([^;]+);/)
+    status = status[1].strip if status && status[1]
+    STATUS_MAP[status] || KIND_MAP[wp.style]
   end
 end
